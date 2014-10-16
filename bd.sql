8e83e84c5c32ca52aca5de1e20e31b3aad0227a3
linux <linux@linux-ThinkPad-T420>
2014-10-16 12:03:56 -0300

    todos campos banco de dados

--- bd.sql
+++ bd.sql
@@ -5,7 +5,14 @@ create table usuario (
     senha varchar(255),
     sexo char,
     cpf varchar(15),
-    endereco varchar(100)
-
-
+    rg varchar(15),
+    endereco varchar(100),
+    cep varchar(10),
+    cidade varchar(50),
+    estado varchar(50),
+    nome varchar(20),
+    datanasc Date,
+    celular varchar(10),
+    telefone varchar(10)
+    
 );
