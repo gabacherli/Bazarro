fa81024e301ef1a6dad3b48b54553fe9b04f2929
thomazpicelli <thomazpicelli@gmail.com>
2014-11-04 20:39:05 -0200

    atualização banco.sql - tabela de carrinho

--- banco.sql
+++ banco.sql
@@ -1,5 +1,3 @@
-/* home/linux/projetos... mysqldump Bazarro -u root -plinux > banco.sql*/;
-
 -- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: Bazarro
@@ -16,6 +14,30 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `carrinho`
+--
+
+DROP TABLE IF EXISTS `carrinho`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `carrinho` (
+  `codusu` int(11) DEFAULT NULL,
+  `valor` float DEFAULT NULL,
+  KEY `codusu` (`codusu`),
+  CONSTRAINT `codusu` FOREIGN KEY (`codusu`) REFERENCES `usuario` (`codusu`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carrinho`
+--
+
+LOCK TABLES `carrinho` WRITE;
+/*!40000 ALTER TABLE `carrinho` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carrinho` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `endereco`
@@ -59,6 +81,7 @@ CREATE TABLE `produto` (
   `valor` float DEFAULT NULL,
   `imagem` varchar(200) DEFAULT NULL,
   `descricao` varchar(200) DEFAULT NULL,
+  `carrinho` bit(1) DEFAULT NULL,
   PRIMARY KEY (`codpro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -112,4 +135,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-10-23 11:48:31
+-- Dump completed on 2014-11-04 19:57:39
